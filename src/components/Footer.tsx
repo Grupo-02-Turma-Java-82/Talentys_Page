fe8d02d6a8f6046b05cd3f54906d86651acbe5c5
Lívia D'Alexandri Cunha <liviadlxcunha@gmail.com>
2025-07-03 14:15:59 -0300

    :tada: Implementação da footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,13 @@
-export function Footer() {
+import React from "react";
+
+const Footer: React.FC = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <footer className="bg-[#0D1B2A] text-[#CBD5E0] text-xs py-4">
+      <div className="text-center">
+        &copy; {new Date().getFullYear()} Talentys. Todos os direitos reservados.
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
